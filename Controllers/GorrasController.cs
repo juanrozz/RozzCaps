4dc7a43c7c79702845342b9d7c78489bd4a14546
juanrozz <juandavidrozovargas2@gmail.com>
2026-07-16 23:32:43 -0500

    ultima actualizacion a la busqueda (mayor eficiencia)

--- Controllers/GorrasController.cs
+++ Controllers/GorrasController.cs
@@ -5,6 +5,8 @@ using RozzCaps.DTOs.Response;
 using RozzCaps.DTOs.Resquest;
 using RozzCaps.Entidades;
 using RozzCaps.Extensiones;
+using System.Text.RegularExpressions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace RozzCaps.Controllers
 {
@@ -15,7 +17,7 @@ namespace RozzCaps.Controllers
         private readonly RozzCapsContext _dbContext;
 
         public GorrasController(RozzCapsContext dbContext)
-        { 
+        {
             _dbContext = dbContext;
         }
 
@@ -30,7 +32,8 @@ namespace RozzCaps.Controllers
 
             Categoria? existe = await _dbContext.Categorias.FirstOrDefaultAsync(e => e.Id == request.Categoria);
 
-            if (existe == null) {
+            if (existe == null)
+            {
                 return BadRequest("Esta categoria no existe paisano");
             }
             if (existe.Activo == false)
@@ -39,7 +42,7 @@ namespace RozzCaps.Controllers
             }
 
             Gorra data = request.ToEntity();
-                
+
             await _dbContext.AddAsync(data);
             await _dbContext.SaveChangesAsync();
 
@@ -60,7 +63,7 @@ namespace RozzCaps.Controllers
                 .ThenInclude(a => a.GorraImagenes)
                 .ToListAsync();
 
-            if(gorras.Count <= 0)
+            if (gorras.Count <= 0)
             {
                 return BadRequest("Error al obtener las gorras");
             }
@@ -78,50 +81,64 @@ namespace RozzCaps.Controllers
                 return BadRequest(new { Mensaje = "Ingresa un término para realizar la búsqueda." });
             }
 
-            string busqueda = request.Trim().ToLower();
-            string raiz = busqueda.Length >= 4 ? busqueda.Substring(0, 3) : busqueda;
+            string textoLimpio = request.Trim();
+            string busquedaTalla = Regex.Replace(textoLimpio, @"\s+", "").ToLower(); // busqueda talla
             // Intentar verificar si el usuario ingresó un número (para buscar por ID)
-            bool esNumero = int.TryParse(busqueda, out int idBuscado);
+            bool esNumero = int.TryParse(textoLimpio, out int idBuscado);
 
-            List<Gorra> resultados = await _dbContext.Gorras
+            string[] palabras = textoLimpio.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); // se sepra por terminos las palabras
+
+            IQueryable<Gorra> query =  _dbContext.Gorras
                 .Include(c => c.Categoria)
                 .Include(g => g.GorraVariaciones.Where(v => v.Activo))
-                .ThenInclude(v => v.GorraImagenes)
-                .Where(g => g.Activo && (
-                    (esNumero && g.Id == idBuscado) || // por el ID
+                    .ThenInclude(v => v.GorraImagenes)
+                .Include(g => g.GorraVariaciones)
+                    .ThenInclude(c => c.Color);
 
-                    g.Nombre.ToLower().Contains(busqueda) || // por el Nombre
-                    g.Nombre.ToLower().Contains(raiz) ||
+            if (esNumero)
+            {
+                query = query.Where(g => g.Id == idBuscado);// se prioriza la busqueda exacta por id
+            }
+            else
+            {
+            
+                foreach (var palabra in palabras)
+                {
+                    string palabraActual = palabra.ToLower();
+                    string raiz = palabraActual.Length >= 4 ? palabraActual.Substring(0, 3) : palabraActual;
 
-                    (g.Categoria != null && (g.Categoria.Nombre.ToLower().Contains(busqueda) || // por el nombre de la categoria
-                    g.Categoria.Nombre.ToLower().Contains(raiz)
-                    )) ||
+                    query = query.Where(g =>
+                        g.Nombre.Contains(palabraActual) ||
+                        g.Nombre.Contains(raiz) ||
+                        (g.Categoria != null && (g.Categoria.Nombre.Contains(palabraActual) || 
+                            g.Categoria.Nombre.Contains(raiz))) ||
+                        g.GorraVariaciones.Any(v =>
+                            v.Activo && (
+                                (v.Color != null && (v.Color.Nombre.Contains(palabraActual) || 
+                                    v.Color.Nombre.Contains(raiz))) ||
+                                (busquedaTalla == "ajustable" && v.Talla == null) ||
+                                (busquedaTalla == "cerrada" && v.Talla != null) ||
+                                (v.Talla != null && v.Talla.Contains(palabraActual)) ||
+                                (v.Talla != null && v.Talla.Contains(busquedaTalla))
 
-                    g.GorraVariaciones.Any(v => v.Color != null && (v.Color.Nombre.ToLower().Contains(busqueda) || // por color
-                    v.Color.Nombre.ToLower().Contains(raiz) ||
+                            )
+                        )
+                    );
+                }
+            }
 
-                    g.GorraVariaciones.Any(t =>
-                    (busqueda == "ajustable" && v.Talla == null) ||
-                    (busqueda == "cerrada" && v.Talla != null) ||
-                    t.Talla != null && (v.Talla.ToLower() == busqueda))
+            List<Gorra> resultados = await query.Where(g => g.Activo).ToListAsync();
 
-                    ))
-                     
-                ))
-
-                .ToListAsync();
-
-            if (resultados.Count == 0){
-
-                return NotFound($"No se encontraron resultados para {request}");
+            if (resultados.Count == 0)
+            {
+                return NotFound(new { Mensaje = $"No se encontraron resultados para '{request}'" });
             }
 
             List<CrearGorraResponseDto> response = resultados.Select(g => g.ToResponseDto()).ToList();
-
             return Ok(response);
         }
 
-       
+
 
     }
 }
